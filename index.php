82dcc15d3f66062fdb0d8f8f748f2050e4c44f1a
MaxWayborn <55049724+MaxWayborn@users.noreply.github.com>
2023-07-04 18:56:41 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$r = explode('/', $_SERVER['REQUEST_URI']);
+switch ($r[1])
+{
+    case 'page1':
+        $htmlTitle = '111';
+        $pageTitle = '111';
+        $pageTitle = ' some php code as string ';
+        include('template.html');
+        break;
+  
+    case 'page2':
+        $htmlTitle = '222';
+        $pageTitle = '222';
+        $pageTitle = ' some php code as string ';
+        include('template.html');
+        break;
+  
+    default:
+        $htmlTitle = '404';
+        $pageTitle = '404';
+        http_response_code(404);
+        include('template.html');
+        break;
+}
